8103cba4280ef0450e1d55bc870bb3a2cad3dba8
nikhilmn7 <nikhil.mn7@gmail.com>
2024-11-15 16:41:19 -0800

    pushed updated unit tests

--- test.cpp
+++ test.cpp
@@ -1,1 +1,46 @@
 #include "gtest/gtest.h"
+#include "healthItem.h"
+#include "attackItem.h"
+#include "defenseItem.h"
+
+TEST(ItemTests, healthItemConstructor){
+    healthItem item1("item1");
+    EXPECT_EQ(item1.getName(), "item1");
+    EXPECT_EQ(item1.getAbility(), "health");
+    EXPECT_EQ(item1.getStrength(), 10);
+}
+
+TEST(ItemTests, healthItemAbility){
+    Character* player1 = new Character;
+    healthItem item1("item1");
+    item1.useAbility(player1);
+    EXPECT_EQ(player1->getHP(), 110);
+}
+
+TEST(ItemTests, attackItemConstructor){
+    attackItem item1("item1");
+    EXPECT_EQ(item1.getName(), "item1");
+    EXPECT_EQ(item1.getAbility(), "attack");
+    EXPECT_EQ(item1.getStrength(), 10);
+}
+
+TEST(ItemTests, attackItemAbility){
+    Character* player1 = new Character;
+    attackItem item1("item1");
+    item1.useAbility(player1);
+    EXPECT_EQ(player1->getAttack(), 20);
+}
+
+TEST(ItemTests, defenseItemConstructor){
+    defenseItem item1("item1");
+    EXPECT_EQ(item1.getName(), "item1");
+    EXPECT_EQ(item1.getAbility(), "defense");
+    EXPECT_EQ(item1.getStrength(), 10);
+}
+
+TEST(ItemTests, defenseItemAbility){
+    Character* player1 = new Character;
+    defenseItem item1("item1");
+    item1.useAbility(player1);
+    EXPECT_EQ(player1->getDefense(), 20);
+}
