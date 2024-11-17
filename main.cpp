a6d0b7d5ea5cbd606df28bc8189f18c7bcbd2bf0
LaithOmar04 <laithomar2104@gmail.com>
2024-11-17 12:53:02 -0800

    updated CMakeLists, test.cpp, and inventory class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+#include "item.h"
+#include "healthItem.h"
+#include "attackItem.h"
+#include "defenseItem.h"
+#include "character.h"
+#include "inventory.h"
+
+using namespace std;
+
 int main() {
+
     
+
     return 0;
 }
